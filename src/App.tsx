8c490f618ea2d977997f6973e5c49a7f5990a327
akshaywritescode <teamakshayyadav@gmail.com>
2025-11-03 17:47:32 +0530

    basic hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 export default function App() {
   return (
-    <main className="flex items-center justify-center h-screen bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold mb-2">⚡ CraftAPI</h1>
-      <p className="text-gray-400">Electron</p>
-
-      <button className="mt-6 px-4 py-2 bg-blue-600 hover:bg-blue-500 rounded-lg font-medium">
-        Get Started
-      </button>
+    <main className="w-full min-h-screen bg-black">
+      <h1>Hello World</h1>
     </main>
   );
 }
